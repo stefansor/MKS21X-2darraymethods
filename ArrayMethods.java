d2ee8c7ac8970b335cc598848904b819d6d727dd
stefansor <ssorobay00@stuy.edu>
2018-11-06 00:40:00 -0500

    parts 1,2, and 3 tested and completed

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,6 +1,9 @@
 public class ArrayMethods{
   public static int rowSum(int[][]ary, int x){
     int sum = 0;
+    if(x >= ary.length){
+      return 0;
+    }
     for(int i = 0; i < ary[x].length; i++){
       sum = sum + ary[x][i];
     }
@@ -14,5 +17,46 @@ public class ArrayMethods{
       }
     }
     return sum;
+  }
+  public static int[] allRowSums(int[][] ary){
+    int[] sums = new int[ary.length];
+    for(int i = 0; i < ary.length; i++){
+      sums[i] = rowSum(ary, i);
+    }
+    return sums;
+  }
+  public static int[] allColSums(int[][] ary){
+    int longest = 0;
+    for(int i = 0; i < ary.length; i++){
+      if(longest < ary[i].length){
+        longest = ary[i].length;
+      }
+    }
+    int[] sums = new int[longest];
+    for(int j = 0; j < longest; j++){
+      sums[j] = columnSum(ary, j);
+    }
+    return sums;
+  }
+  public static boolean isRowMagic(int[][] ary){
+    int plate = allRowSums(ary)[0];
+    for(int i = 0; i < allRowSums(ary).length; i++){
+      if(allRowSums(ary)[i] != plate){
+        return false;
+      }
+    }
+    return true;
+  }
+  public static boolean isColumnMagic(int[][] ary){
+    if(allColSums(ary).length == 0){
+      return true;
+    }
+    int plate = allColSums(ary)[0];
+    for(int i = 0; i < allColSums(ary).length; i++){
+      if(allColSums(ary)[i] != plate){
+        return false;
+      }
+    }
+    return true;
   }
 }
